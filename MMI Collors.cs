70fcacc46fdeea27bdc679997f8a69e27f880f10
BIM-Lord <96540939+BIM-Lord@users.noreply.github.com>
2023-09-25 19:34:24 +0200

    Update MMI Collors.cs

--- MMI Collors.cs
+++ MMI Collors.cs
@@ -74,38 +74,3 @@ namespace MultiNodes
 
 
 
-/*
-using System;
-using System.Collections.Generic;
-using Autodesk.DesignScript.Runtime;
-using DSCore;
-
-namespace MultiNodes
-{
-public class MMIFarger
-
-	{
-	private MMIFarger() { }
-
-	/// <summary>
-	/// Generate random color
-	/// </summary>
-	/// <param name="amount">Number of color want to generate</param>
-	/// <param name="start">start of range color(0-255)</param>
-	/// <param name="end">end of range color(0-255)</param>
-	/// <returns name="colors">list colors random</returns>
-	public static List<DSCore.Color> Random(double amount, double start = 0, double end = 255)
-		{
-		// return list color by number of amount with random color shift is 2
-		var listColor = new List<DSCore.Color>();
-		for (int i = 0; i < amount; i++)
-			{
-			listColor.Add(DSCore.Color.ByARGB(255, (byte)DSCore.Math.Random(start, end),
-				(byte)DSCore.Math.Random(start, end), (byte)DSCore.Math.Random(start, end)));
-			}
-
-		return listColor;
-		}
-	}
-}
-*/
